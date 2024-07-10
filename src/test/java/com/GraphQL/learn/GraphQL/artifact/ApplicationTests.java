871b0b180105744e90e26a0830552cbb463ba3c1
sanan <sanan3946@gmail.com>
2024-07-10 19:27:58 +0530

    project init

---
+++ src/test/java/com/GraphQL/learn/GraphQL/artifact/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GraphQL.learn.GraphQL.artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
